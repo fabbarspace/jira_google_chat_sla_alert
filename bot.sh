cd6f3a823eb177d27833c60d08caf1e0d3f12309
Fabricio Barros <63207264+fabriciolbarros@users.noreply.github.com>
2023-10-09 10:55:41 -0300

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-node /home/user_name/jira_google_chat_sla_alert/sekiro_bot_sla_jira.js >> /home/user_name/jira_google_chat_sla_alert/log_sekiro_bot_sla_jira.log
+node /home/user_name/jira_google_chat_sla_alert/bot_sla_jira.js >> /home/user_name/jira_google_chat_sla_alert/log_bot_sla_jira.log
